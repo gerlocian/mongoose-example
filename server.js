6993e26f68626a1052dfaeed26fe7157244e4137
Patrick Ortiz <pogithub@gmail.com>
2023-06-13 12:32:32 -0400

    added simple database logic to server.js

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const http = require('http');
 const server = http.createServer();
 const mongoose = require('mongoose');
 
-server.on('request', (req, res) => {
-    res.end('OK');
-});
+async function main() {
+    await mongoose.connect('mongodb://admin:Password1@mongo:27017/test?authSource=admin');
 
-server.listen(3000, 'localhost', () => console.log('started...'));
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+
+    const Kitten = mongoose.model('Kitten', kittySchema);
+
+    const misty = new Kitten({ name: 'Misty' });
+    const fluffy = new Kitten({ name: 'Fluffy' });
+
+    await misty.save();
+    await fluffy.save();
+
+    const kittens = await Kitten.find();
+    console.log(kittens);
+}
+
+main().catch(err => console.log(err));
